ec6242e77c493c73dd60c7307ede058b1aa31bf4
SHIVENDRAIYER <shivendrav13@gmail.com>
2021-03-24 10:05:28 +0530

    Test cases running

--- tms/src/test/java/com/cg/tms/packagems/service/PackageServiceUnitTestImpl.java
+++ tms/src/test/java/com/cg/tms/packagems/service/PackageServiceUnitTestImpl.java
@@ -13,6 +13,8 @@ import com.cg.tms.packagems.util.*;
 import com.cg.tms.packagems.repository.*;
 import org.junit.jupiter.api.Assertions;
 import com.cg.tms.packagems.entities.Package;
+import org.junit.jupiter.api.function.Executable;
+import com.cg.tms.packagems.exceptions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PackageServiceUnitTestImpl {
@@ -26,23 +28,63 @@ public class PackageServiceUnitTestImpl {
 	@InjectMocks
 	PackageServiceImpl packageService;
 
+	/**
+	 * success scenario : Add package successfully
+	 */
 	@Test
 	public void testAdd_Package1() {
 
-		String packageName = "Holiday";
-		String packageDescription = "peace and relaxation";
-		doNothing().when(packageService).validatePackageName(packageName);
-		doNothing().when(packageService).validatePackageDescription(packageDescription);
-		Package reserved = Mockito.mock(Package.class);
-		Package pack = Mockito.mock(Package.class);
-		when(packageUtil.newPackage()).thenReturn(pack);
-		when(packageRepository.save(pack)).thenReturn(reserved);
+		/*
+		 * String packageName = "Holiday"; String packageDescription =
+		 * "peace and relaxation";
+		 * doNothing().when(packageService).validatePackageName(packageName);
+		 * doNothing().when(packageService).validatePackageDescription(
+		 * packageDescription); Package reserved = Mockito.mock(Package.class); Package
+		 * pack = Mockito.mock(Package.class);
+		 * when(packageUtil.newPackage()).thenReturn(pack);
+		 * when(packageRepository.save(pack)).thenReturn(reserved); Package result =
+		 * packageService.addPackage(pack); Assertions.assertNotNull(result);
+		 * Assertions.assertEquals(reserved, result);
+		 * verify(packageRepository).save(pack);
+		 * verify(packageService).validatePackageDescription(packageDescription);
+		 * verify(packageService).validatePackageName(packageDescription);
+		 */
+
+		int packageId = 2;
+		String packageName = "International";
+		String packageDescription = "challenging adventure";
+		String packageType = "Ultra";
+		double packageCost = 10500.0;
+		Package pack = new Package(packageId, packageName, packageDescription, packageType, packageCost);
 		Package result = packageService.addPackage(pack);
 		Assertions.assertNotNull(result);
-		Assertions.assertEquals(reserved, result);
-		verify(packageRepository).save(pack);
-		verify(packageService).validatePackageDescription(packageDescription);
-		verify(packageService).validatePackageName(packageDescription);
+		Assertions.assertEquals(packageId, result.getPackageId());
+		Assertions.assertEquals(packageName, result.getPackageName());
+		Assertions.assertEquals(packageDescription, result.getPackageDescription());
+		Assertions.assertEquals(packageType, result.getPackageType());
+		Assertions.assertEquals(packageCost, result.getPackageCost());
+
+	}
+
+	/**
+	 * Scenario: PackageName Validation - Blank input
+	 * 
+	 */
+	@Test
+	public void testAdd_Package2() {
+
+		/*
+		 * String packageName = "";
+		 * doThrow(InvalidPackageNameException.class).when(packageService).
+		 * validatePackageName(packageName); Executable executable = () ->
+		 * packageService.addPackage();
+		 * Assertions.assertThrows(InvalidPackageNameException.class, executable);
+		 */
+
+		String packageName = "";
+		Package pack = new Package(1, packageName, "diverse and cultural", "Normal", 8500.0);
+		Executable executable = () -> packageService.addPackage(pack);
+		Assertions.assertThrows(InvalidPackageNameException.class, executable);
 
 	}
 
